870dfb67aa7d924268c93042335a574693b631bb
Michal Mastej <m.mastej1812@gmail.com>
2025-09-04 15:51:08 +0200

    Small refactor and added more robots

--- mpl_widgets/robot_sim.py
+++ mpl_widgets/robot_sim.py
@@ -5,7 +5,7 @@ mpl.use("Qt5Agg")
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 
-from mpl_widgets.robots import robot0, positions0, robot1, positions1, robot2, positions2
+from mpl_widgets.robots import robot0, positions0, robot1, positions1, robot2, positions2, robot3, positions3, robot4, positions4
 
 
 
@@ -17,23 +17,18 @@ class RobotSimulation(FigureCanvas):
         super().__init__(figure)
 
         self.board_size = 15
-        self.position_idxs = [1, 1, 1]
-        self.forbidden_positions = [(0, 0), (1, 1), (3, 3)]
+        self.position_idxs = [1, 1, 1, 1, 1]
+        self.forbidden_positions = [(0, 0), (1, 1), (3, 3), (12, 12), (14, 14)]
 
-        self.robot0 = robot0
-        self.robot1 = robot1
-        self.robot2 = robot2
-        self.positions0 = positions0
-        self.positions1 = positions1
-        self.positions2 = positions2
+        self.robots = [robot0, robot1, robot2, robot3, robot4]
+        self.positions = [positions0, positions1, positions2, positions3, positions4]
 
         for x in range(self.board_size + 1):
             self.ax.axhline(x, color="gray", linewidth=0.5)
             self.ax.axvline(x, color="gray", linewidth=0.5)
 
-        self.ax.add_patch(self.robot0)
-        self.ax.add_patch(self.robot1)
-        self.ax.add_patch(self.robot2)
+        for robot in self.robots:
+            self.ax.add_patch(robot)
 
         self.ax.set_xlim(0, self.board_size)
         self.ax.set_ylim(0, self.board_size)
@@ -47,31 +42,25 @@ class RobotSimulation(FigureCanvas):
     def start_moving(self) -> None:
         self.timer.start(200)
 
+    def reset_pos_idx(self, robot_idx):
+        if self.position_idxs[robot_idx] == len(self.positions[robot_idx]):
+            self.position_idxs[robot_idx] = 0
+
+    def move_robot(self, robot_idx):
+        if (self.positions[robot_idx][self.position_idxs[robot_idx]] not in self.forbidden_positions):
+            self.forbidden_positions.remove(self.robots[robot_idx].get_xy())
+            self.forbidden_positions.append(self.positions[robot_idx][self.position_idxs[robot_idx]])
+            self.robots[robot_idx].set_xy(self.positions[robot_idx][self.position_idxs[robot_idx]])
+            self.position_idxs[robot_idx] += 1
+
     def update_position(self) -> None:
         # for i in range(len(self.position_idxs)):
         #     self.position_idxs[i] += 1
-        if self.position_idxs[0] == len(self.positions0):
-            self.position_idxs[0] = 0
-        if self.position_idxs[1] == len(self.positions1):
-            self.position_idxs[1] = 0
-        if self.position_idxs[2] == len(self.positions2):
-            self.position_idxs[2] = 0
-        if (self.positions0[self.position_idxs[0]] not in self.forbidden_positions):
-            self.forbidden_positions.remove(self.robot0.get_xy())
-            self.forbidden_positions.append(self.positions0[self.position_idxs[0]])
-            self.robot0.set_xy(self.positions0[self.position_idxs[0]])
-            self.position_idxs[0] += 1
-        if (self.positions1[self.position_idxs[1]] not in self.forbidden_positions):
-            self.forbidden_positions.remove(self.robot1.get_xy())
-            self.forbidden_positions.append(self.positions1[self.position_idxs[1]])
-            self.robot1.set_xy(self.positions1[self.position_idxs[1]])
-            self.position_idxs[1] += 1
-        if (self.positions2[self.position_idxs[2]] not in self.forbidden_positions):
-            self.forbidden_positions.remove(self.robot2.get_xy())
-            self.forbidden_positions.append(self.positions2[self.position_idxs[2]])
-            self.robot2.set_xy(self.positions2[self.position_idxs[2]])
-            self.position_idxs[2] += 1
 
-        # self.robot2.set_xy(self.positions2[self.position_idxs[1]])
-        # self.robot3.set_xy(self.positions3[self.position_idxs[2]])
+        for i in range(len(self.robots)):
+            self.reset_pos_idx(i)
+
+        for i in range(len(self.robots)):
+            self.move_robot(i)
+
         self.draw()
